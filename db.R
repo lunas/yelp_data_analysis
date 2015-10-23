eb766897cef51adf8237d180d3856f637bb00b6b
lunas <lukasnick@gmail.com>
2015-10-23 11:21:19 +0200

    play around w/ friends reviewing businesses and igraph
    Read friends-max-groups-reviewing businesses from pg-db, make a couple
    of plots.
    
    Load igraph, create friendsgraph. Running the community detection
    algorhithm made my computer almost crash with memory and disk exhaustion.

--- db.R
+++ db.R
@@ -25,3 +25,6 @@ query <- function(sql) {
   return ( dbSendQuery(connect(), sql) )
 }
 
+db.write.table <- function(dataframe, tablename) {
+  dbWriteTable(connect(), name=tablename, value=dataframe, overwrite=TRUE, row.names=FALSE)
+}
